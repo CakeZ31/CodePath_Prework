2a2bafb79698d0dbb17e577d0ed08b7f835d2d3b
CakeZ31 <51308530+CakeZ31@users.noreply.github.com>
2021-01-12 18:41:51 -0800

    Add files via upload
    CodePath Prework

---
+++ CodePath_Prework/Prework1/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Prework1
+//
+//  Created by Brandon Ortega on 1/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    @IBOutlet weak var billAmountTextField: UITextField!
+    
+    @IBOutlet weak var tipPercentageLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func onTap(_ sender: Any) {
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        let bill = Double(billAmountTextField.text!) ?? 0
+        let tipPercentages = [0.15, 0.18, 0.2]
+        
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        
+        let total = bill + tip
+        
+        tipPercentageLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
+}
+
